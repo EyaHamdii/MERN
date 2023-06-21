59ca98adaacded2db4e1e7c22502725631bae892
Eya Hamdi <eyahamdi184@gmail.com>
2023-06-21 15:43:01 +0100

    First_React

--- Week 1/Day 2/Practice/It's time to duel/Cards.js
+++ Week 1/Day 2/Practice/It's time to duel/Cards.js
@@ -10,9 +10,29 @@ class Unit extends Card {
         this.power=power;
         this.res=res;
     }
+    attack(target){
+        `${target.res}-${this.power}`
+    }
+    play( target ) {
+            if( target instanceof Unit ) {
+                // implement card text here
+            } else {
+                throw new Error( "Target must be a unit!" );
+            }
+        }
+        
 }
 class Effect extends Card {
-    constructor(name, cost){
+    constructor(name, cost, stat, magnitude){
         super(name,cost)
+        this.stat=stat
+        this.magnitude=magnitude
+        `${this.magnitude} the target's ${this.stat} by ${this.magnitude}`
     }
+
 }
+const ninja1 =new Unit('Red Belt Ninja',3,3,4);
+const ninja2 =new Unit('Red Belt Ninja',4,5,4);
+const effect1 =new Effect('Hard Algorithm',2 ,resilience,+3);
+const effect2 =new Effect('Unhandled Promise Rejection',1,resilience,-2);
+const effect3 =new Effect('Pair Programming',3,power, +2);
